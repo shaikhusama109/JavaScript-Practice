562caa79ee8922f7ee9afa0900df917d1da61210
Usama <shaikhusama109@gmail.com>
2022-02-07 12:39:27 +0500

    commited

---
+++ Java Script/setFeildVal.js
@@ -0,0 +1,11 @@
+function fill(){
+    document.getElementById("f").value = "Lauria";
+   }
+
+   
+
+   function fillStatus() {
+    if (document.getElementById("married").value === "no") {
+      document.getElementById("status").value = "available";
+    }
+  }
---
+++ Java Script/setParaText.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    var var1 = document.getElementById("p1").innerHTML;
+      alert(var1);
+   }
+
+
+   function showAnswer(){
+    document.getElementById("p1").innerHTML = "4"
+   }
