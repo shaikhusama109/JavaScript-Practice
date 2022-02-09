6e97b4d09abae95017b9666e9669e8ff4973cd3a
abdul <abdul@gmail.com>
2022-02-09 11:15:33 +0500

    Commited

---
+++ Java Script/bulbOnOff.js
@@ -0,0 +1,9 @@
+function bulbOn(){
+    var img = document.getElementById("img")
+    img.src = "Images/bulbon.png"
+}
+
+function bulbOff(){
+    var img = document.getElementById("img")
+    img.src = "Images/bulboff.png"
+}
