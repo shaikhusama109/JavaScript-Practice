a65fa517fbf865681de01fbdeb9dd9d1673dfbd7
Usama Ahmed <shaikhusama109@gmail.com>
2022-02-13 21:04:49 +0500

    Commited

---
+++ Java Script/counter.js
@@ -0,0 +1,9 @@
+function add(){
+    var val = document.getElementById("counter")
+    val.innerHTML ++ 
+}
+
+function reset(){
+    var val = document.getElementById("counter")
+    val.innerHTML = 0
+}
