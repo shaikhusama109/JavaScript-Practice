dcb761b90e8604ffea91b84a08cd8a80e3d30778
Usama Ahmed <shaikhusama109@gmail.com>
2022-02-15 11:10:10 +0500

    Commited

---
+++ Java Script/objInArr.js
@@ -0,0 +1,20 @@
+var myQuestions = [
+    {
+        question : "what is your name",
+        answer : {
+            a : "ali",
+            b : "ghous",
+            c : "basit"
+        },
+        correctAnswer : "c"
+    },
+    {
+        question : "what is your age",
+        answer : {
+            a : 15,
+            b : 12,
+            c : 16
+        },
+        correctAnswer : "b"
+    },
+]
