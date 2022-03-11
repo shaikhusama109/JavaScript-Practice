a648247f9cf9d399e93b30cfd7e50b86232319a3
Usama Ahmed <shaikhusama109@gmail.com>
2022-03-11 20:39:17 +0500

    Commited

--- Java Script/webchange.js
+++ Java Script/webchange.js
@@ -1,4 +1,17 @@
 var currentSite = window.location.hostname;
-2 var currentPath = window.location.pathname;
-3 var destination = "http://" + currentSite + currentPath + "#humidifier";
-4 window.location.href = destination;
+ var destination = "http://" + currentSite + "/wow.html";
+ window.location.href = destination;
+
+var currentSite = window.location.hostname;
+ var currentPath = window.location.pathname;
+ var destination = "http://" + currentSite + currentPath + "#humidifier";
+ window.location.href = destination;
+
+ window.location.replace("http://www.me.com/lojack.html#guarantee");
+
+ window.location.assign("http://www.me.com/lojack.html#guarantee");
+
+
+ window.location.reload(true);
+window.location.reload(false);
+window.location.reload();
