884043e6710a5378dc9087430d3d437db9cadbcf
Usama Ahmed <shaikhusama109@gmail.com>
2022-03-19 20:55:53 +0500

    Commited

--- Java Script/saylaniEs6.js
+++ Java Script/saylaniEs6.js
@@ -164,6 +164,15 @@
 
 // getData(render_data);
 
+// hello = () => {
+//     document.getElementById("demo").innerHTML += this;
+//   }
+  
+//   // The window object calls the function:
+//   window.addEventListener("load", hello);
+  
+//   // A button object calls the function:
+//   document.getElementById("btn").addEventListener("click", hello);
 
 //  Array Filter 
 
