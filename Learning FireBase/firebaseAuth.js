eab9e71bf6e4d5de47f638b44f0a896b8214a3a6
Usama Ahmed <shaikhusama109@gmail.com>
2022-03-20 12:44:14 +0500

    Commitedf

---
+++ Learning FireBase/firebaseAuth.js
@@ -0,0 +1,32 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyD6lb_oYLhUU-nub4uNNuZOR-ABavgR_5k",
+    authDomain: "learning-db-1c4cc.firebaseapp.com",
+    databaseURL: "https://learning-db-1c4cc-default-rtdb.firebaseio.com",
+    projectId: "learning-db-1c4cc",
+    storageBucket: "learning-db-1c4cc.appspot.com",
+    messagingSenderId: "169720787281",
+    appId: "1:169720787281:web:076c779c1850fbc064e370"
+  };
+  
+    firebase.initializeApp(firebaseConfig);
+ 
+
+let signup = () =>{
+    let email = document.getElementById("email")
+    let password = document.getElementById("password")
+
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage)
+    // ..
+  });
+}
