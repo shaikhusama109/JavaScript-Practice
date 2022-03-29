9130e6092c24d95bd65164067761016306480a3e
Usama shaikh <shaikhusama109@gmail.com>
2022-03-29 20:41:10 +0500

    Commited

---
+++ Problem Solving/app.js
@@ -0,0 +1,9 @@
+//How would you check if a number is an integer
+
+function isInt(num) {
+    return num % 1 === 0;
+  }
+  
+  console.log(isInt(4)); // true
+  console.log(isInt(12.2)); // false
+  console.log(isInt(0.3)); // false
