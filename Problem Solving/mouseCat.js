eff7a213ff9d7d75b5a8a05e08337350d08ecf12
Usama shaikh <shaikhusama109@gmail.com>
2022-11-09 20:58:38 +0500

    Commited

---
+++ Problem Solving/mouseCat.js
@@ -0,0 +1,21 @@
+// Hacker Rank two cats catching mouse, the cat who have less distance will catch the mouse first if both had the same distance mouse could escape
+
+function catAndMouse(x, y, z) {
+    let distCat1 = Math.abs(z-x);
+    let distCat2 = Math.abs(z-y);
+   
+    
+    if(distCat1 < distCat2){
+        return 'Cat A';
+    }
+    else if(distCat2 < distCat1){
+        return 'Cat B';
+    }
+    else{
+        return 'Mouse C';
+    }
+    
+   
+   }
+
+   console.log(catAndMouse(10,10,0));
