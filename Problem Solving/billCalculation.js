199cdc4851434f8a2c16bd878b2663bd7ef8fed2
shaikhusama109 <shaikhusama109@gmail.com>
2022-11-30 14:38:36 +0500

    Commited

---
+++ Problem Solving/billCalculation.js
@@ -0,0 +1,29 @@
+let bill = [3, 10, 2, 9]
+let k = 1
+let b = 14
+
+function bonAppetit(bill, k, b) {
+    // Write your code here
+    let totalBill = 0;  
+    let annasCut = 0;
+
+      
+      for(let i = 0; i< bill.length; i++){
+          totalBill += bill[i];
+        
+        }
+        
+    annasCut = (totalBill - bill[k])/2
+         
+        if(b == annasCut){
+            console.log("Bon Appetit")
+        }
+        else{
+            console.log( b - annasCut)
+        }
+            
+            
+}
+
+bonAppetit(bill,k,b);
+
---
+++ Problem Solving/minmaxCount.js
@@ -0,0 +1,21 @@
+let scores = [10, 5, 20 ,20, 4, 5 ,2, 25 ,1]
+
+
+    let minCount = 0;
+    let maxCount = 0;
+    let minValue = scores[0];
+    let maxValue = scores[0];
+     
+        for(let i=0; i<=scores.length;i++){
+            if(scores[i] > maxValue){
+                maxValue = scores[i];
+                maxCount ++;
+            }
+            else if(scores[i] < minValue){
+                minValue = scores[i]
+                minCount ++;                
+            }
+        }
+        
+        console.log(maxCount);
+        console.log(minCount);
