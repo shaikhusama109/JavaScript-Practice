c5e63852086b9ccfc46f00402a7bacc293a98ad3
shaikhusama109 <shaikhusama109@gmail.com>
2023-01-08 20:22:54 +0500

    Commited

---
+++ Java Script/constructor2.js
@@ -0,0 +1,15 @@
+class MyBook{
+    constructor(title,author,price){
+      this.title = title;
+      this.author = author;
+      this.price = price;  
+    }
+    display(){
+        console.log(`Title: ${this.title}`)
+        console.log(`Author: ${this.author}`)
+        console.log(`Price: ${this.price}`)
+    }
+    // Write your method here
+    
+// End class
+    }
