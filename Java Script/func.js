147969554cc942261c11552c2d1431996eeca17d
shaikhusama109 <shaikhusama109@gmail.com>
2023-01-16 20:54:44 +0500

    Commited

--- Java Script/func.js
+++ Java Script/func.js
@@ -1,9 +1,13 @@
 //Function Implementation
 
-function data(fname, lname) {
+//function data(fname, lname) {
 
-    console.log('My Name is : ' + fname + ' ' + lname);
+//     console.log('My Name is : ' + fname + ' ' + lname);
 
+// }
+
+const data = (fName, lName) => {
+    console.log(`My name is ${fName} ${lName}`)
 }
 data('Usama', 'Shaikh')
 data('Abdul', 'Basit')
