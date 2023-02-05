fb5d049b88714f754f69f5396c95fe936c6031c3
shaikhusama109 <shaikhusama109@gmail.com>
2023-02-05 23:06:26 +0500

    Commited

--- Java Script/webchange.js
+++ Java Script/webchange.js
@@ -2,16 +2,16 @@ var currentSite = window.location.hostname;
  var destination = "http://" + currentSite + "/wow.html";
  window.location.href = destination;
 
-// var currentSite = window.location.hostname;
-//  var currentPath = window.location.pathname;
-//  var destination = "http://" + currentSite + currentPath + "#humidifier";
-//  window.location.href = destination;
+var currentSite = window.location.hostname;
+ var currentPath = window.location.pathname;
+ var destination = "http://" + currentSite + currentPath + "#humidifier";
+ window.location.href = destination;
 
-//  window.location.replace("http://www.me.com/lojack.html#guarantee");
+ window.location.replace("http://www.me.com/lojack.html#guarantee");
 
-//  window.location.assign("http://www.me.com/lojack.html#guarantee");
+ window.location.assign("http://www.me.com/lojack.html#guarantee");
 
 
-//  window.location.reload(true);
-// window.location.reload(false);
-// window.location.reload();
+ window.location.reload(true);
+window.location.reload(false);
+window.location.reload();
