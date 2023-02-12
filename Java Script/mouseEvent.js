25a79bbb4afdeb6c215e257b2c6efca28058c118
shaikhusama109 <shaikhusama109@gmail.com>
2023-02-12 19:46:17 +0500

    Commited

--- Java Script/mouseEvent.js
+++ Java Script/mouseEvent.js
@@ -1,8 +1,8 @@
-alert('Please Place Your Mouse Over the Image');
+// alert('Please Place Your Mouse Over the Image');
 
-function over(){
-    alert('Jooooooooooooooood')
-}
-function out(){
-    alert('Your Mouse is not over the Image')
-}
+// function over(){
+//     alert('Jooooooooooooooood')
+// }
+// function out(){
+//     alert('Your Mouse is not over the Image')
+// }
