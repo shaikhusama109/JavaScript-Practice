55affbd47522b9c273565e4373083f2e9151a31f
shaikhusama109 <shaikhusama109@gmail.com>
2023-03-11 23:24:17 +0500

    Commiteed

--- Problem Solving/nazPlaza.js
+++ Problem Solving/nazPlaza.js
@@ -1,29 +1,29 @@
-// A person wants to determine the most expensive computer keyboard and USB drive that 
-// can be purchased with a give budget. Given price lists for keyboards 
-// and USB drives and a budget, find the cost to buy them.
-//  If it is not possible to buy both items, return .
+// // A person wants to determine the most expensive computer keyboard and USB drive that 
+// // can be purchased with a give budget. Given price lists for keyboards 
+// // and USB drives and a budget, find the cost to buy them.
+// //  If it is not possible to buy both items, return .
 
-let b = 10
-let keyboards = [3, 1]
-let drives = [5, 2, 8]
+// let b = 10/
+// let keyboards = [3, 1]
+// let drives = [5, 2, 8]
 
-function getMoneySpent(keyboards, drives, b) {
-    /*
-     * Write your code here.
-     */
-    let maxValue = 0
+// function getMoneySpent(keyboards, drives, b) {
+//     /*
+//      * Write your code here.
+//      */
+//     let maxValue = 0
 
-        for (const keyboard of keyboards) {
-            for (const drive of drives) {
-                const sum = keyboard + drive
-                if (maxValue < sum && sum <= b) {
-                    maxValue = sum
-                }
-            }
-        }
+//         for (const keyboard of keyboards) {
+//             for (const drive of drives) {
+//                 const sum = keyboard + drive
+//                 if (maxValue < sum && sum <= b) {
+//                     maxValue = sum
+//                 }
+//             }
+//         }
 
-        if (maxValue == 0) {
-            return -1
-        }
-        return maxValue;
-    }
+//         if (maxValue == 0) {
+//             return -1
+//         }
+//         return maxValue;
+//     }
