b2a8141bb54957eee195834a5bdb67943692de02
shaikhusama109 <shaikhusama109@gmail.com>
2023-03-17 23:16:53 +0500

    Commited

--- Problem Solving/qurban.js
+++ Problem Solving/qurban.js
@@ -1,22 +1,22 @@
-function angryProfessor(k, a) {
-    // Write your code here
-    let onTime = 0;
-    for(let i=0; i<a.length; i++){
-        if(a[i] <= 0){
-            onTime++
-            console.log(onTime)
-            console.log(a[i])
+// function angryProfessor(k, a) {
+//     // Write your code here
+//     let onTime = 0;
+//     for(let i=0; i<a.length; i++){
+//         if(a[i] <= 0){
+//             onTime++
+//             console.log(onTime)
+//             console.log(a[i])
           
-        }
-    }
-    if((onTime >= k)){
-        return("NO")
-    }
-    else{
-        return("YES")
-    }
+//         }
+//     }
+//     if((onTime >= k)){
+//         return("NO")
+//     }
+//     else{
+//         return("YES")
+//     }
 
 
-}
+// }
 
-angryProfessor(3,[-1,-3,4,2])
+// angryProfessor(3,[-1,-3,4,2])
