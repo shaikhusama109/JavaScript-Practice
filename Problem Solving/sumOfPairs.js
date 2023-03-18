f255d871258304dd9b79ff920afb60c04a40d29d
shaikhusama109 <shaikhusama109@gmail.com>
2023-03-18 08:21:30 +0500

    Commited

--- Problem Solving/sumOfPairs.js
+++ Problem Solving/sumOfPairs.js
@@ -1,22 +1,22 @@
-// // Given an array of integers and a positive integer "k" 
-// // , determine the number of(i,j)  pairs where (i<j)and ar[i] + ar[j] is 
-// // divisible by "k".
+// Given an array of integers and a positive integer "k" 
+// , determine the number of(i,j)  pairs where (i<j)and ar[i] + ar[j] is 
+// divisible by "k".
 
 
-// let ar= [1,2,3,4,5,6];
-// let n = ar.length;
-// let k = 5;
+let ar= [1,2,3,4,5,6];
+let n = ar.length;
+let k = 5;
 
-// function divisibleSumPairs(n, k, ar) {
-//     // Write your code here
-//     let noOfPairs = 0;
-//     for (let i=0; i<n; i++){
-//         for (let j= 0; j<n; j++){
-//             let sumOfPairs = ar[i] + ar[j];
-//             if( i < j && sumOfPairs % k == 0){
-//                 noOfPairs ++
-//             }
-//         }
-//     }
-//         return noOfPairs;
-// }
+function divisibleSumPairs(n, k, ar) {
+    // Write your code here
+    let noOfPairs = 0;
+    for (let i=0; i<n; i++){
+        for (let j= 0; j<n; j++){
+            let sumOfPairs = ar[i] + ar[j];
+            if( i < j && sumOfPairs % k == 0){
+                noOfPairs ++
+            }
+        }
+    }
+        return noOfPairs;
+}
