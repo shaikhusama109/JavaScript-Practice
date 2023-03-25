70a4f2b71ae3a333b3b490c77700b30763420923
shaikhusama109 <shaikhusama109@gmail.com>
2023-03-25 22:00:46 +0500

    rebuild fixes

--- Problem Solving/minusPlus.js
+++ Problem Solving/minusPlus.js
@@ -1,22 +1,22 @@
-let arr = [4, -9, 0, 9, -4];
-let plusVal = 0;
-let negVal = 0;
-let zero = 0;
-let arrLength = arr.length;
+// let arr = [4, -9, 0, 9, -4];
+// let plusVal = 0;
+// let negVal = 0;
+// let zero = 0;
+// let arrLength = arr.length;
 
-for (let i = 0; i < arrLength; i++) {
-  if (arr[i] > 0) {
-    plusVal++;
-  } else if (arr[i] < 0) {
-    negVal++;
-  } else {
-    zero++;
-  }
-}
-let posAns = (plusVal / arrLength).toFixed(6);
-let negAns = (negVal / arrLength).toFixed(6);
-let zeroAns = (zero / arrLength).toFixed(6);
+// for (let i = 0; i < arrLength; i++) {
+//   if (arr[i] > 0) {
+//     plusVal++;
+//   } else if (arr[i] < 0) {
+//     negVal++;
+//   } else {
+//     zero++;
+//   }
+// }
+// let posAns = (plusVal / arrLength).toFixed(6);
+// let negAns = (negVal / arrLength).toFixed(6);
+// let zeroAns = (zero / arrLength).toFixed(6);
 
-console.log(posAns);
-console.log(negAns);
-console.log(zeroAns);
+// console.log(posAns);
+// console.log(negAns);
+// console.log(zeroAns);
