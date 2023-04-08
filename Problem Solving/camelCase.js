5b261c77cad3769ced122d5766dfbe7748a21b6d
shaikhusama109 <shaikhusama109@gmail.com>
2023-04-08 20:43:11 +0500

    rebuild fixes

--- Problem Solving/camelCase.js
+++ Problem Solving/camelCase.js
@@ -1,12 +1,12 @@
-// https://www.hackerrank.com/challenges/camelcase/problem?isFullScreen=true
+// // https://www.hackerrank.com/challenges/camelcase/problem?isFullScreen=true
 
-function camelcase(s) {
-    let noOfCamelCase=1;
-    for(let i = 0; i < s.length; i++){
-        if( s.charCodeAt(i) >= 65 && s.charCodeAt(i)<=90 ){
-            noOfCamelCase++
-        }
-    }
-    return(noOfCamelCase)
+// function camelcase(s) {
+//     let noOfCamelCase=1;
+//     for(let i = 0; i < s.length; i++){
+//         if( s.charCodeAt(i) >= 65 && s.charCodeAt(i)<=90 ){
+//             noOfCamelCase++
+//         }
+//     }
+//     return(noOfCamelCase)
 
-}
+// }
