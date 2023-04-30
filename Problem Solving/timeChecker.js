0c48deb576521beffff7ba9d8532a3eed94b4855
shaikhusama109 <shaikhusama109@gmail.com>
2023-04-30 15:01:19 +0500

    Rebuild Fixes

--- Problem Solving/timeChecker.js
+++ Problem Solving/timeChecker.js
@@ -1,22 +1,22 @@
-// https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
+// // https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
 
-function timeConversion(s) {
+// function timeConversion(s) {
     
-    let hrs = s.slice(0, 2)
-    let timeSpan = s.slice(8);
+//     let hrs = s.slice(0, 2)
+//     let timeSpan = s.slice(8);
 
 
-    if (timeSpan == 'PM' && Number(hrs) < 12) {
-        hrs = Number(hrs) + 12
-        // return hrs
-    } else if (timeSpan == 'AM' && Number(hrs) == 12) {
-        hrs = "0" + (Number(hrs) - 12)
-        // return hrs
-    } else {
-        hrs = hrs
-        // return hrs
-    }
+//     if (timeSpan == 'PM' && Number(hrs) < 12) {
+//         hrs = Number(hrs) + 12
+//         // return hrs
+//     } else if (timeSpan == 'AM' && Number(hrs) == 12) {
+//         hrs = "0" + (Number(hrs) - 12)
+//         // return hrs
+//     } else {
+//         hrs = hrs
+//         // return hrs
+//     }
 
-    let result = hrs + s.slice(2, 8)
-    return result;
-}
+//     let result = hrs + s.slice(2, 8)
+//     return result;
+// }
